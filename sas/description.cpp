e5af40ec9c010173f661ae836de2bd0027d250c0
Bart <bp.login1@gmail.com>
2026-03-01 15:14:39 +0100

    Add initial project structure and guidelines for SAS framework

---
+++ sas/description.cpp
@@ -0,0 +1,12 @@
+//-->SAS Functions
+#ifdef sas_cfg_functions
+class SAS_Morale {
+    class morale {
+        file = "sas\morale\functions";
+        class calculateGroupFear {};
+        class getGroupFear {};
+        class eventHandlerGroupFear {};
+    };
+};
+#endif
+//<--SAS Functions
